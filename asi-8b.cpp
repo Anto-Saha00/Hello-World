31f93af697ff38048951644ca74221359b5e4e63
Anto-Saha00 <106462405+Anto-Saha00@users.noreply.github.com>
2022-05-30 23:18:52 +0600

    Add files via upload

---
+++ asi-8b.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+
+ int x,y,mul;
+
+ cout<<" Enter the value of x: ";
+ cin>>x;
+
+ cout<<" Enter the value of y: ";
+ cin>>y;
+
+ cout<<"\n";
+ mul=x*y;
+
+ cout<<" The multiplication is "<<mul;
+
+  return 0;
+}
