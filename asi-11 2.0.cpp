2bbfc3ef6353edfb525b0898e9d638f9674cf131
Anto-Saha00 <106462405+Anto-Saha00@users.noreply.github.com>
2022-05-30 23:20:20 +0600

    Add files via upload

---
+++ asi-11 2.0.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+
+ float marks;
+ float results;
+
+ cout<<" Enter the marks of a student: ";
+ cin>>marks;
+
+ results=((marks/200)*100);
+ cout<<" The percentage of marks: "<<results;
+
+
+return 0;
+}
