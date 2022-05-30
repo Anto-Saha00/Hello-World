52603abfea13e09231ef0424663ea6125a5529bb
Anto-Saha00 <106462405+Anto-Saha00@users.noreply.github.com>
2022-05-30 23:17:53 +0600

    Add files via upload

---
+++ asi-6.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+
+ int a,b;
+ float sum,sub,mul,div;
+
+ cout<<" Enter the value of a ";
+ cin>>a;
+
+ cout<<" Enter the value of b ";
+ cin>>b;
+
+ cout<<"\n";
+ sum=a+b;
+ sub=a-b;
+ mul=a*b;
+ div=a/b;
+
+ cout<<" The summation of the integer is: "<<sum<<endl;
+ cout<<" The subtraction of the integer is: "<<sub<<endl;
+ cout<<" The multiplication of the integer is: "<<mul<<endl;
+ cout<<" The division of the integer is : "<<div<<endl;
+
+  return 0;
+}
