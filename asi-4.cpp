c47c88855efc4006f8cbca4e2b3b8c1edc3e3284
Anto-Saha00 <106462405+Anto-Saha00@users.noreply.github.com>
2022-05-30 23:16:57 +0600

    Add files via upload

---
+++ asi-4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+
+ int x,y,z, sum=0;
+
+ cout<<" Enter the value of x "<<endl;
+ cin>>x;
+
+ cout<<" Enter the value of y "<<endl;
+ cin>>y;
+
+ cout<<" Enter the value of z "<<endl;
+ cin>>z;
+
+ sum=x+y+z;
+ cout<<" The summation of the integer is: "<<sum;
+
+  return 0;
+}
