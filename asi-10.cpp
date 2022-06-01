097181bb1c4ad6fc780d3eaac3916ed923c459d1
Anto-Saha00 <106462405+Anto-Saha00@users.noreply.github.com>
2022-06-01 11:16:36 +0600

    Add files via upload

---
+++ asi-10.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+    float area,radius;
+    cout<< "Input your radius";
+
+    cin>>radius;
+
+    area=  (3.1416*(radius*radius));
+
+cout<< "The area of circle is: "<<radius<<endl;
+
+
+
+  return 0;
+}
